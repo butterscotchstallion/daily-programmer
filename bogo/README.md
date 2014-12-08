af4b5022e2a74ce6a5cc8b8e4d2fdc19e85b6cc5
PrgmrBill <bill+github@prgmrbill.com>
2014-12-07 23:02:55 -0500

    Initial commit

---
+++ bogo/README.md
@@ -0,0 +1,16 @@
+=== Bogo Sort ===
+A bogo sort is a purposefully inefficient algorithm for sorting a sequence. Today we will be using this for strings to test for equality.
+
+=== Rules ===
+Given a scrambled string N and another string M. You must sort N so that it matches M. After it has been sorted, it must output how many iterations it took to complete the sorting.
+
+== Sample Inputs & Outputs ==
+
+Input:
+Bogo("lolhe","Hello")
+
+Output:
+1456 iterations
+
+== Bonus ==
+For a bit of fun, the LEAST efficient algorithm wins. Check out the bogo-bogo sort, an algorithm that's designed not to succeed before the heat death of the universe
---
+++ bogo/bogo.go
@@ -0,0 +1,69 @@
+/**
+ * bogosort - Given a scrambled string N and another string M. You must 
+ * sort N so that it matches M. After it has been sorted, it must output 
+ * how many iterations it took to complete the sorting.
+ *
+ * For a bit of fun, the LEAST efficient algorithm wins. Check out the bogo-bogo sort, 
+ * an algorithm that's designed not to succeed before the heat death of the universe
+ * http://www.dangermouse.net/esoteric/bogobogosort.html[2]
+ *
+ *
+ */
+package main
+
+import (
+		"fmt"
+		"strings"
+		"math/rand"
+		"log"
+		"time"
+		"os"
+)
+
+func main () {
+		rand.Seed(time.Now().UTC().UnixNano())
+		
+		if len(os.Args) != 3 {
+			log.Fatal("Usage: bogo helol hello")
+		}
+		
+		firstWord := os.Args[1]
+		secondWord := os.Args[2]
+		
+		bogoSortAndReturnIterations(firstWord, secondWord)
+}
+
+func bogoSortAndReturnIterations(scrambled string, unscrambled string) int {
+		letters := strings.Split(scrambled, "")
+
+		var iterations int
+
+		for {
+			shuffledStringSlice := ShuffleStringSlice(letters)
+			word 				:= strings.Join(shuffledStringSlice, "")
+			
+			iterations++
+			
+			// Compare word to original input
+			if word == unscrambled {
+				log.Print(fmt.Sprintf("Shuffled word with %d iterations.", iterations))
+				
+				break
+			} else {
+				log.Print(fmt.Sprintf("Iteration #%d: %s", iterations, word))
+			}
+		}
+		
+		return iterations
+}
+
+func ShuffleStringSlice (input []string) []string {
+	for i := range input {
+		j := rand.Intn(i + 1)
+		input[i], input[j] = input[j], input[i] 
+	}
+	
+	return input
+}
+
+
